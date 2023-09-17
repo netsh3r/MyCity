1b2cdf8d4cb546f7a66731b36d0f36b425549acc
dmitry <d.shiriaev@bars.group>
2023-09-17 14:35:38 +0400

    Update RouteService.cs

--- MyCity.Route.Service/Services/RouteService.cs
+++ MyCity.Route.Service/Services/RouteService.cs
@@ -63,7 +63,7 @@ public class RouteService : IRouteService
                 await UpdateRouteAsync(clientRouteDto) :
                 await CreateRouteAsync(clientRouteDto);
             
-            await _repositoryRoutePoints.в(new RoutePoints
+            await _repositoryRoutePoints.UpdateAsync(new RoutePoints
             {
                 RouteId = route.Id,
                 RoutePointsObj = JsonConvert.SerializeObject(clientRouteDto.RoutePoints)
